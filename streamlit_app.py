c57d8badd9718b5bacc4a31eac9b3b8b63433e04
SintiyaRistiyani <113277452+SintiyaRistiyani@users.noreply.github.com>
2025-07-10 22:48:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,22 +197,22 @@ elif menu == "Stasioneritas":
     from scipy.stats import skew, kurtosis, shapiro, jarque_bera
 
     def diagnostik_saham(series, nama_saham):
-    st.markdown(f"## 🧪 Uji Diagnostik Distribusi: {nama_saham}")
-    series = series.dropna()
+        st.markdown(f"## 🧪 Uji Diagnostik Distribusi: {nama_saham}")
+        series = series.dropna()
+        
+        # Skewness & Kurtosis
+        skw = skew(series)
+        krt = kurtosis(series)
+        st.write(f"**Skewness:** {skw:.4f}")
+        st.write(f"**Kurtosis:** {krt:.4f}")
     
-    # Skewness & Kurtosis
-    skw = skew(series)
-    krt = kurtosis(series)
-    st.write(f"**Skewness:** {skw:.4f}")
-    st.write(f"**Kurtosis:** {krt:.4f}")
-
-    # Visualisasi histogram + KDE
-    fig, ax = plt.subplots(figsize=(10, 4))
-    sns.histplot(series, kde=True, bins=30, color='skyblue', ax=ax)
-    ax.set_title(f'Distribusi Log Return {nama_saham}')
-    ax.set_xlabel('Log Return')
-    ax.set_ylabel('Frekuensi')
-    st.pyplot(fig)
+        # Visualisasi histogram + KDE
+        fig, ax = plt.subplots(figsize=(10, 4))
+        sns.histplot(series, kde=True, bins=30, color='skyblue', ax=ax)
+        ax.set_title(f'Distribusi Log Return {nama_saham}')
+        ax.set_xlabel('Log Return')
+        ax.set_ylabel('Frekuensi')
+        st.pyplot(fig)
 
 # =============================== MODEL ==============================
 elif menu == "Model":
